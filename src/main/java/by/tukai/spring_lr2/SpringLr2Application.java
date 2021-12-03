209f3e16b3b0fcb05c16053c9c2022dd418e0fb4
Elena <tukaialena2@gmail.com>
2021-12-03 16:35:45 +0300

    Add web config

---
+++ src/main/java/by/tukai/spring_lr2/SpringLr2Application.java
@@ -0,0 +1,14 @@
+package by.tukai.spring_lr2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLr2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLr2Application.class, args);
+
+    }
+
+}
